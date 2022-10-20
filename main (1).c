cb19aa3df58cb75f7ed840e9c88823660d1ad642
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-10-20 00:07:55 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+{
+float casamento,nome, sim, nao,michael,jessica;
+printf("digite seu nome:");
+scanf("%f",&nome);
+    printf("peça a Jéssica em casamento:");
+    //Jéssica gosta de coisas fofas e romanticas, pois ela te ama e o que vc fizer sempre vai ser algo especial!
+return 0;
+}
